dafbf9adf4a0375f42abdaad82f6918deefceed0
unknown <vk6529599@gmail.com>
2026-07-18 14:47:26 +0530

    halfdiamond

---
+++ halfdiamondinvertedpattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size = 5;
+    for(int i = 5; i>=0; i--){
+        for(int j = 0; j<=size-i; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j<(2*i-1); j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}    
--- halfdiamondpattern.cpp
+++ halfdiamondpattern.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 int main(){
     int size = 5;
-    for(int i = 5; i>=0; i--){
-        for(int j = 0; j<=size-i; j++){
-            cout<<" ";
-        }
-        for(int j = 0; j<(2*i-1); j++){
+    for(int i = 0; i<5; i++){
+        for(int j = 0; j<=i; j++){
             cout<<"*";
         }
         cout<<endl;
     }
-}    
+    for(int i = size-1; i>0; i--){
+        for(int j = 0; j<i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }    
+}
